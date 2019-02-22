18ff8d7607aff00d41b34592dc921b2dd0e5de32
BalaRajendran <karurbalamathi@gmail.com>
2019-02-22 20:31:36 +0530

    updayed

--- index.py
+++ index.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template,request
+import mysql.connector
 
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+from flaskext.mysql import MySQL
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] ="root"
+app.config['MYSQL_DATABASE_PASSWORD'] = ""
+app.config['MYSQL_DATABASE_DB'] ="railway"
+app.config['MYSQL_DATABASE_HOST'] = "localhost"
+mysql.init_app(app)
+con=mysql.connect()
+cursor=con.cursor()
 @app.route('/')
-def hello_name():
-   return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-   app.run(debug = True)
+@app.route('/store',methods=['POST', 'GET'])
+def store():
+   if request.method=='POST':
+      fname=request.form['fname']
+      lname=request.form['lname']
+      from1=request.form['from1']
+      to1=request.form['to1']
+      date=request.form['date']
+      type1=request.form['type1']
+      count=request.form['count']
+      class1=request.form['class1']
+      email=request.form['email']
+      tel=request.form['tel']
+      payment=request.form['payment']
+      if(cursor.execute('''insert into railway (fname,lname,from1,to1,date,type1,count,class1,email,tel,payment) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',(fname,lname,from1,to1,date,type1,count,class1,email,tel,payment))): 
+         con.commit()
+         return render_template('index.html',response = 'Ticked Booked Sucessfully')
+      return render_template('index.html',response = 'Something Wrong')
+   else:
+      return render_template('index.html',response = 'Something Wrong')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
