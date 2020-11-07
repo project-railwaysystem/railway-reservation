21feca8af15f2b9a5385ad07b86268532f772403
BalajiRajendranPurpleSlate <balaji.r@purpleslate.in>
2020-11-07 23:17:15 +0530

    fix: production fixes

--- index.py
+++ index.py
@@ -45,4 +45,4 @@ def store():
       return render_template('index.html',response = 'Something Wrong')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
