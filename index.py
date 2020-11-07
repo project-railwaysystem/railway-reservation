03fca274eff32dfd3dd4680c6e8cdaa31d70555d
BalajiRajendranPurpleSlate <balaji.r@purpleslate.in>
2020-11-07 23:12:05 +0530

    feat: catch handler & cdn fixes

--- index.py
+++ index.py
@@ -4,14 +4,17 @@ import mysql.connector
 app = Flask(__name__, static_url_path="", static_folder="static")
 
 from flaskext.mysql import MySQL
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] ="epiz_21548430"
-app.config['MYSQL_DATABASE_PASSWORD'] = "Yni4VOUXQ7l0"
-app.config['MYSQL_DATABASE_DB'] ="epiz_21548430_rail_reservation"
-app.config['MYSQL_DATABASE_HOST'] = "sql106.epizy.com"
-mysql.init_app(app)
-con=mysql.connect()
-cursor=con.cursor()
+try:
+   mysql = MySQL()
+   app.config['MYSQL_DATABASE_USER'] ="id15343073_railway"
+   app.config['MYSQL_DATABASE_PASSWORD'] = "[%!|O)uhs*73veLk"
+   app.config['MYSQL_DATABASE_DB'] ="id15343073_railway_reservation"
+   app.config['MYSQL_DATABASE_HOST'] = "localhost"
+   mysql.init_app(app)
+   con=mysql.connect()
+   cursor=con.cursor()
+except Exception as e:
+   print("Error while connecting to MySQL", e)
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -19,23 +22,27 @@ def index():
 @app.route('/store',methods=['POST', 'GET'])
 def store():
    if request.method=='POST':
-      fname=request.form['fname']
-      lname=request.form['lname']
-      from1=request.form['from1']
-      to1=request.form['to1']
-      date=request.form['date']
-      type1=request.form['type1']
-      count=request.form['count']
-      class1=request.form['class1']
-      email=request.form['email']
-      tel=request.form['tel']
-      payment=request.form['payment']
-      if(cursor.execute('''insert into railway (fname,lname,from1,to1,date,type1,count,class1,email,tel,payment) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',(fname,lname,from1,to1,date,type1,count,class1,email,tel,payment))): 
-         con.commit()
-         return render_template('index.html',response = 'Ticked Booked Sucessfully')
-      return render_template('index.html',response = 'Something Wrong')
+      try:
+         fname=request.form['fname']
+         lname=request.form['lname']
+         from1=request.form['from1']
+         to1=request.form['to1']
+         date=request.form['date']
+         type1=request.form['type1']
+         count=request.form['count']
+         class1=request.form['class1']
+         email=request.form['email']
+         tel=request.form['tel']
+         payment=request.form['payment']
+         if(cursor.execute('''insert into railway (fname,lname,from1,to1,date,type1,count,class1,email,tel,payment) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',(fname,lname,from1,to1,date,type1,count,class1,email,tel,payment))): 
+            con.commit()
+            return render_template('index.html',response = 'Ticked Booked Sucessfully')
+         else:
+            return render_template('index.html',response = 'Something Wrong')
+      except Exception as e:
+         return render_template('index.html',response = 'Something Wrong')
    else:
       return render_template('index.html',response = 'Something Wrong')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
